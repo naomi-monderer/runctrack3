1e2feab26dafc62b7382c5d54f548ecba6940448
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2022-03-17 15:50:02 +0100

    job02 terminé

---
+++ Jour4/Job02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <p>Créez une fonction javascript “jsonValueKey()” qui prend en paramètre une
+chaîne de caractères au format json et une clé. Cette fonction retourne la
+valeur liée à cette clé dans la chaîne de caractères. Par exemple si la
+string passée en paramètre est “{name: "La Plateforme_", address: "8 rue
+
+d'hozier", city: "Marseille", nb_staff: "11", creation:"2019"}” et la clé est
+“city”, la fonction retourne “Marseille”. </p>
+    <button id="button"> click me</button>
+</body>
+</html>
---
+++ Jour4/Job02/script.js
@@ -0,0 +1,28 @@
+
+    document.addEventListener("DOMContentLoaded",(event)=>
+    {
+        json =     
+        {
+            "name": "LaPlateform",
+            "address": "8 rue d'Hozier",
+            "city": "Marseille",
+            "nb_staff": 11,
+            "creation":"2019"
+        }
+        
+       function jsonValueKey(value, key)
+       { 
+           console.log(value.key + 'value.key soit value."city"')
+           console.log(value.city + 'value.key soit value.city')
+           console.log(value[key] + 'value.key soit value.city')
+           return value[key]
+
+       }
+      let result = jsonValueKey(json,"city")
+      console.log(result)
+
+      console.log(jsonValueKey(json,"city"))
+
+    })
+   
+   
