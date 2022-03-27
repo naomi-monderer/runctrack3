3893abfedc68af60317dc13fb92e91436eb1e1ac
naomie-monderer <92308722+naomie-monderer@users.noreply.github.com>
2022-03-27 22:28:01 +0200

    burgger button

---
+++ Jour6/Job05/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <!-- <script src="main.js"></script> -->
+    <title>Document</title>
+</head>
+ 
+<header>
+    <nav>
+        <!-- <div class="main_page"> -->
+            <label for="toggle">☰</label>
+            <input type="checkbox" id="toggle">
+            <div class="main_pages">
+                <a href="#">Accueil</a>
+                <a href="#">Actualité</a>
+                <a href="#">Contact</a>
+            </div>
+       
+    </nav>
+</header>
+<body>
+    
+</body>
+</html>
